1057e98da091261ae83ff88e0e4cb761007e77d2
Tanmay Jana <tanmay.jana@mountblue.io>
2019-10-26 17:58:48 +0530

    added section 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App'
 import LandingPage from './Components/LandingPage/LandingPage'
 import Section1 from './Components/Section1/Section1'
+import Section2 from './Components/Section2/Section2'
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <div className = "App">
         <LandingPage/>
         <Section1/>
+        <Section2/>
       </div>
     )
   }
---
+++ src/Components/Section2/Section2.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./Section2.css";
+import family from "../../assets/family.png";
+import meeting from "../../assets/b-meeting.png";
+import list from "../../assets/filter-organization.png";
+
+export default class Section2 extends Component {
+  render() {
+    return (
+      <div className="section2-container">
+        <h1>The mental wellness revolution</h1>
+        <h2>
+          Having fun online while increasing emotional literacy and emotional
+          intelligence
+        </h2>
+        <div className="features-container">
+          <div className="features">
+            <div className="feature-head">
+              <img src={family} alt="family" />
+              <p>Understand</p>
+            </div>
+            <p className = "feature-body">
+              The first part of the process of developing emotional li teracy
+              and emotional intelligence is to understand what it is that is
+              being felt, and why.
+            </p>
+          </div>
+          <div className="features">
+            <div className="feature-head">
+              <img src={meeting} alt="meeting" />
+              <p>Articulate</p>
+            </div>
+            <p className = "feature-body">
+              Once the emotion and its context is understood the key is to be
+              able to crystalise this into words and articulate the feeling.{" "}
+            </p>
+          </div>
+          <div className="features">
+            <div className="feature-head">
+              <img src={list} alt="liat" />
+              <p>Healthily Navigate</p>
+            </div>
+            <p className = "feature-body">
+              Finally the ability to navigate a full range of emotions will
+              prevent slipping into negative cycles of poor emotional
+              regulation.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
