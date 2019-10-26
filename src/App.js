e7f0f20e00cd2782dea43bc83184e1a801f15112
Tanmay Jana <tanmay.jana@mountblue.io>
2019-10-26 19:01:34 +0530

    addedse section 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App'
 import LandingPage from './Components/LandingPage/LandingPage'
 import Section1 from './Components/Section1/Section1'
 import Section2 from './Components/Section2/Section2'
+import Section3 from './Components/Section3/Section3'
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <LandingPage/>
         <Section1/>
         <Section2/>
+        <Section3/>
       </div>
     )
   }
---
+++ src/Components/Section3/Section3.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./Section3.css";
+import mock from '../../assets/MOCK.png'
+
+export default class Section3 extends Component {
+  render() {
+    return (
+      <div className="section3-container">
+        <p className="section3-header">How does it work?</p>
+        <div className="blue-box">
+          <div className="work-lists">
+            <div className="work-list">
+              <h2>Share</h2>
+              <p>
+                Moodit makes is super easy and intuitive to share emotions and
+                the context behind them.
+              </p>
+            </div>
+            <div className="work-list">
+              <h2>Normalise</h2>
+              <p>
+                Moodit will advise how many people feel similar and thus
+                normalise the emotions felt.
+              </p>
+            </div>
+            <div className="work-list">
+              <h2>Moodhacks</h2>
+              <p>
+                Moodit will suggest relevant Moodhacks to the emotions being
+                felt to give crowdsourced mood boosting tips that are up/down
+                voted by each user.
+              </p>
+            </div>
+          </div>
+          <img className = "mobile1-img" src = {mock} alt = "mobile"/>
+        </div>
+      </div>
+    );
+  }
+}
