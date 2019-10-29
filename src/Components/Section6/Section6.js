8d73e3770ccd40dfdf5c1a06092cb4ca73cfef54
Tanmay Jana <tanmay.jana@mountblue.io>
2019-10-29 16:05:37 +0530

    adjusted icons size in section 6

--- src/Components/Section6/Section6.js
+++ src/Components/Section6/Section6.js
@@ -4,7 +4,7 @@ import bitmap from '../../assets/Bitmap.png';
 import vmLogo from '../../assets/VM-Logo-black.png';
 import googleLogo from '../../assets/Google.png';
 import ukLogo from '../../assets/Uk.png';
-import segmentLogo from '../../assets/Segment.png';
+import segmentLogo from '../../assets/Segment1.png';
 import fbLogo from '../../assets/FB1.png';
 import websummitLogo from '../../assets/websummit-logo.png'
 
@@ -15,10 +15,10 @@ export default class Section6 extends Component {
                 <p className = "credibility-header">Recognised and supported by</p>
                 <div className = "logo-container">
                 <img className = "logo-img" src = {bitmap} alt = "bitmap" />
-                <img className = "logo-img" src = {vmLogo} alt = "vmLogo" />
+                <img className = "logo-img big-logo" src = {vmLogo} alt = "vmLogo" />
                 <img className = "logo-img" src = {googleLogo} alt = "google" />
                 <img className = "logo-img" src = {ukLogo} alt = "uk" />
-                <img className = "logo-img" src = {segmentLogo} alt = "segment_logo" />
+                <img className = "logo-img big-logo" src = {segmentLogo} alt = "segment_logo" />
                 <img className = "logo-img" src = {fbLogo} alt = "fb" />
                 <img className = "logo-img" src = {websummitLogo} alt = "websummit" />
                 </div>
